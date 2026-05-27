8488de1eb6882ea75ae648e879621752604579cb
Subhash <126864268+subhash0299@users.noreply.github.com>
2026-05-27 23:07:46 +0530

    Restrict clear chat to POST requests
    Add method check for clear chat endpoint

--- api/clear-chat.js
+++ api/clear-chat.js
@@ -1,5 +1,11 @@
 module.exports = (req, res) => {
 
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      success: false
+    });
+  }
+
   const { password } = req.body;
 
   if (password === process.env.CLEAR_PASSWORD) {
