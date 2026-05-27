eb469f0006f3789052dffb58fe362f06a3112e8b
Subhash <126864268+subhash0299@users.noreply.github.com>
2026-05-27 23:03:45 +0530

    Change export syntax to CommonJS for clear-chat.js

--- api/clear-chat.js
+++ api/clear-chat.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
 
   const { password } = req.body;
 
@@ -11,4 +11,4 @@ export default function handler(req, res) {
   return res.status(401).json({
     success: false
   });
-}
+};
